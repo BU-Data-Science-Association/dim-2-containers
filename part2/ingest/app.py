a89681e1ad9dcb6702f8460a4f647c31b5cb1f4e
clow427 <shawnlau427@gmail.com>
2026-02-09 18:14:57 -0500

    ingest setup

---
+++ part2/ingest/app.py
@@ -0,0 +1,35 @@
+import requests
+import psycopg2
+import os
+import time
+
+# DB Setup
+conn = psycopg2.connect(
+    host=os.getenv("DB_HOST"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASS"),
+    dbname=os.getenv("DB_NAME"),
+)
+
+cur = conn.cursor()
+
+cur.execute("""
+CREATE TABLE IF NOT EXISTS data (
+    id SERIAL PRIMARY KEY,
+    value TEXT
+)
+""")
+
+while True:
+    # Grab data from API
+    data = requests.get("http://api.open-notify.org/iss-now.json").json()
+
+    # Process for table
+    value = str(data["iss_position"])
+
+    # Input to DB
+    cur.execute("INSERT INTO data(value) VALUES (%s)", (value,))
+    conn.commit()
+    
+    # Wait 5 seconds before next poll
+    time.sleep(5)
